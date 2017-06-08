34ecebff5beb0cb0054f871329e6e62a7b923156
BogdanRaicevic <raicevic.bogdan@gmail.com>
2017-06-08 14:58:31 +0200

    intergrating es lint in project, this way plugin is not needed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import numeral from 'numeral';
-
+/* eslint-disable no-console no-debugger*/
 const courseValue = numeral(1000).format('$0,0.00');
 debugger;
 console.log(`I would pay ${courseValue} fro this awesome course!`);
