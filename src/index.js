3cb3c9dd4a2d5c8704c35cd25a52db167a617eb2
BogdanRaicevic <raicevic.bogdan@gmail.com>
2017-06-09 12:02:28 +0200

    Api setUp succesfull

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import './index.css';
-import numeral from 'numeral';
-/* eslint-disable no-console no-debugger*/
-const courseValue = numeral(1000).format('$0,0.00');
-debugger;
-console.log(`I would pay ${courseValue} fro this awesome course!`);
+import { getUsers } from './api/userApi';
+
+//populate tabe of usres via api call
+getUsers().then(result => {
+    let usersBody = "";
+
+    result.forEach(user => {
+        usersBody += `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+        </tr>`
+    });
+
+    global.document.getElementById('users').innerHTML = usersBody;
+})
